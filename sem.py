d046fc9c3be911295f0ea5d1d50ea81a78345d31
Renateitor <renato@carelli.com.ar>
2012-05-04 13:28:28 -0300

    new release

--- sem.py
+++ sem.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
+# SEM (Security Enhanced Messaging) is a collaborative PoC for implementing Cover Channels over ICMP protocol
+#
 # Dependencias: tcpdump, python-scapy
 #
 
-
+# importamos librerias
 import os
 import subprocess
 import time
@@ -13,15 +15,9 @@ import logging
 
 # definimos que solamente se debe alertar ante un error
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
-
 from scapy.all import *
 
-
-
-
-# Levanto los parametros
-
+# Levanto los parametros necesarios para la comunicacion
 target = raw_input('Enter the target device [192.168.1.71]: ')
 passwd = raw_input('Enter the password key for the communication [20121357]: ')
 interface = raw_input('Enter the interface used for the communication (listening) [eth0]: ')
@@ -33,47 +29,43 @@ if passwd == '':
 if interface == '':
 	interface = 'eth0'
 	
-
-
-# Dejo monitoreando en background
-rec_p = subprocess.Popen(['python', 'recivecovert.py','--interface='+interface,'--password='+passwd,'&'])
+# Dejo monitoreando en background para la recepcion de mensajes
+rec_p = subprocess.Popen(['python', 'recive.py','--interface='+interface,'--password='+passwd,'&'])
 rec_pid = rec_p.pid
-
 
 # Loop para chatear
 print 'To exit write: \':q!\''
 txt=raw_input('>> ')
 while txt.strip()!=':q!':
-	
 	txt=txt+'\n'
+	# a partir de aca empieza el armado del paquete y el envio
+
 	# construimos la capa 3 del paquete (IP)
 	l3 = IP()
 	l3.dst = target
+
 	# construimos la capa 4 del paquete (ICMP)
 	l4 = ICMP()
 
 	# definimos el resto de las variables
-	msgsize = 12 # como vamos a dividir el mensaje en partes, aqui definimos el tamano de cada parte
-	#payload = "" # declaramos la variable 'payload' que vamos a utilizar mas adelante
-	# las variables 'first', 'last' y 'count' las vamos a utilizar para el proceso de cada parte del mensaje
+	msgsize = 12 # como vamos a dividir el mensaje en partes, aca definimos el tamano de cada parte
+	# las variables (first) (last) (count) las utilizamos para el proceso de corte y envio del paquete
 	first = 0
 	last = (msgsize)
 	count = (len(txt)/msgsize)+1
-	# entramos en un bucle en el cual vamos a enviar un paquete para cada trozo de datos
+	# entramos en un bucle en el cual vamos a enviar un paquete para cada parte de los datos
 	print "							[ %s : " %(count),
 	for a in range(0, count):
 		print "%s " %(a + 1),
 		payload = passwd + txt[first:last]
-		# ensamblamos el paquete (las capas que no definimos son definidas automaticamente por scapy)
+		# ensamblamos el paquete
+		# las capas que no definimos son definidas automaticamente por scapy
 		pkt = l3/l4/payload
 		# enviamos el paquete
 		a = sr(pkt, verbose = 0, retry = 0, timeout = 1)
 		first += msgsize
 		last += msgsize
 	print ']'
-	
-	
-	#send_p = subprocess.Popen(['python', 'sendcovert.py','--target='+target,'--data='+txt,'--password='+passwd+' &'])	
 	
 	
 	txt=raw_input('>> ')
