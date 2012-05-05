8479974a20487c7cdcce750e05fae6ac566d03bd
Renateitor <renato@carelli.com.ar>
2012-05-04 22:26:24 -0300

    dev

--- recive.py
+++ recive.py
@@ -65,7 +65,7 @@ def monitor_callback(pkt):
 #		print '			<< '+data
 		f = open(archivo, 'r')
 		lastline = f.readlines()[-1]
-		if '\n' in lastline and lastline[0:11] == '[ '+name+' ]:  ':
+		if ('\n' in lastline) and (lastline[0:11] == '[ '+name+' ]:  '):
 			print '1||'+lastline[0:11]+'||1'
 			print '2||'+'[ '+name+' ]:  '+'||2'
 			print '					<< '+lastline[11:]
