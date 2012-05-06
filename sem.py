67aaf1a02113604147fa55cddf8714b00fbcd6c5
renateitor <renato@carelli.com.ar>
2012-05-06 17:13:00 -0300

    under development

--- sem.py
+++ sem.py
@@ -286,7 +286,7 @@ while True:
 
 # Mato el proceso que escucha los paquetes que llegan y los loguea/muestra por pantalla
 os.system('kill -9 '+str(rec_pid))
-#logfilename = str(int(time.time())
-#os.system('kill -9 '+str(rec_pid))
+logfilename = str(int(time.time()))
+os.system('mv message.txt chatlog_'+logfilename[-6:-1]+'.txt')
 print '\n\nGood Bye!\n\n'
 
