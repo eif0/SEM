f09a1f3f47cd02c721f27b0a8a947ace4f9462cf
renateitor <renato@carelli.com.ar>
2012-05-06 04:31:15 -0300

    under development

--- recive.py
+++ recive.py
@@ -123,7 +123,8 @@ def monitor_callback(pkt):
 			print >>f, data,
 			f.close()
 			print '\n\n\n		***[ Se completo la transferencia del archivo ]***'
-			print '		            - Transfer ID: '+tempfile+' -\n\n\n'
+			print '		            - Transfer ID: '+tempfile+' -\n\n'
+			print '		            ( to get the file execute '':conv!'')\n\n\n'
 			tempfile = str(int(time.time()))
 			recibido = '/tmp/'+tempfile
 
