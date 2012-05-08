b22451e194d1cfe1d56392560c356d69cbe064ed
Renateitor <renato@carelli.com.ar>
2012-05-08 18:13:03 -0300

    under development

--- sem.py
+++ sem.py
@@ -223,7 +223,7 @@ def cypher(txt,tipocifrado):
 		return txt
 	if tipocifrado == '1':
 		charnum = 0
-		while charnum < txt.__len__() -2:
+		while charnum < txt.__len__():
 			print txt[charnum]
 			charnum += 1
 		#txt[ch] = chr(ord(txt[ch])+5)
